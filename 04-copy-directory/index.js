42bbd52686e5c62c30c217578e648ddb139c429f
Ksanka Kovsh <ksanka.kovsh@gmail.com>
2022-11-09 10:49:05 +0300

    feate: add solution for task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs/promises");
+const path = require('path');
+const pathField = path.join(__dirname);
 
+const copyField = async () => {
+  let fieldFrom = path.resolve(pathField, "files");
+  let fieldTo = path.resolve(pathField, "files-copy");
+  try {
+    await fs.mkdir(fieldTo, { recursive: true });
+  } catch {
+    console.log("ошибочка вышла");
+  }
+
+  let filesToCopy = await fs.readdir(fieldFrom, {
+    withFileTypes: true,
+  });
+  filesToCopy.forEach((file) => {
+    if (file.isFile()) {
+      const filePath = path.resolve(__dirname, fieldFrom, file.name);
+      fs.copyFile(filePath, path.resolve(fieldTo, file.name));
+      console.log(`сейчас копируется ${file.name}`);
+    }
+  });
+};
+
+copyField();
